95198a4acb419762c64f99007ff52f3cf5706430
yuta sugiyama <yuta@yutanoMacBook-Air.local>
2023-04-03 23:45:57 +0900

    api callとUIの実装

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Card from './components/Card/Card';
+import Navbar from './components/Navbar/Navbar';
+import { getAllPokemon, getPokemon } from './utils/pokemon';
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      let res = await getAllPokemon(initialURL);
+      loadPokemon(res.results)
+      // console.log(res.results);
+      setLoading(false);
+    };
+    fetchPokemonData();
+  }, []);
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData)
+  };
+
+  console.log(pokemonData);
+
   return (
-    <div className="App">
-    </div>
+    <>
+      <Navbar />
+      <div className="App">
+        {loading ? (
+          <h1>ロード中.....</h1>
+        ) : 
+          <div className="pokemonCardContainer">
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon} />;
+            })}
+          </div>
+        }
+      </div>
+    </>
   );
 }
 
